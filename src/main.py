7c25773e5d1232cd43ecdc57f1d70a18038069b2
Matnavarro <matinavarro@gmail.com>
2024-06-13 21:07:09 -0300

    estructura hecha

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
 import pygame
-print("hello world")
+import settings
+
+pygame.init()
+
---
+++ src/settings.py
@@ -0,0 +1,12 @@
+
+
+ANCHO = 10
+ALTO = 600
+SCREEN_SIZE = (ANCHO, ALTO)
+
+# Colors
+
+RED = (255, 0, 0)
+GRENN = (0, 255, 0)
+BLUE = (0, 0, 255)
+CUSTOM = (157, 208, 230)
